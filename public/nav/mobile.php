d8b7bb04dbd5b85532ff6b380691664229b5cb7d
Mitchell Bray <mitchellbray@CPE-124-184-9-209.lns10.cht.bigpond.net.au>
2015-05-27 17:05:08 +1000

    mobile menu fix

--- public/nav/mobile.php
+++ public/nav/mobile.php
@@ -1,18 +1,13 @@
-<?php //Topmenu Code Begins here ?>
 <div class="visible-mobile">
-	<div class="<?php echo $outer_wrapper;?>">
-		<nav class="sm-navbar supermenu animated <?php echo $top_menu_classes;?>" role="navigation">
-			<div class="<?php echo $inner_wrapper;?>">
-			
-				<button type="button" class="sm-navbar-toggle tm-lines-button <?php echo $topmenu_tgl_size;?>" data-toggle="collapse" data-target="#supermenu-sm-navbar-collapse-mob">
+		<nav class="sm-navbar supermenu animated" role="navigation">
+				<button type="button" class="sm-navbar-toggle tm-lines-button" data-toggle="collapse" data-target="#supermenu-sm-navbar-collapse-mob">
 					<span class="lines"></span>
 				</button>
 				
 			
-<!-- 				//Topmenu Image Logo  -->
+<!-- //Topmenu Image Logo  -->
 				<div class="sm-navbar-header">
-					<a class="sm-navbar-brand sm-navbar-brand-primary logo-visible img-logo" href="<?php echo home_url(); ?>"><?php echo $logo ?></a>
-					<a class="sm-navbar-brand sm-navbar-brand-alt img-logo" href="<?php echo home_url(); ?>"><?php echo $logo_alt ?></a>	
+					<img src="<?= $nav_options['logoDesktop']['url']; ?>" />
 				</div><!--.sm-navbar-header-->
 
 				<?php 
@@ -20,8 +15,6 @@
 		
 				//Menu items, bring 'em in
 					wp_nav_menu( array(
-						'menu'              => $active_menu,
-						'theme_location'    => $active_menu,
 						'depth'             => 3,
 						'limit_depth'      => false,
 						'container'         => 'div',
@@ -33,9 +26,6 @@
 						'fallback_cb'       => 'sm_bootstrap_navwalker::fallback',
 						'walker'            => new sm_bootstrap_navwalker())
 					);
-				?>
-			</div><!--.container-->			
+				?>		
 		</nav>
-	</div>
 </div><!--.visible-mobile-->
-<?php //Topmenu Code ends here ?>
