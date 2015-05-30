ec57601d716b4b5cb6a6021101b51c6aef128214
Mitchell Bray <mitchellbray@CPE-58-168-16-236.lns4.cht.bigpond.net.au>
2015-05-30 16:34:27 +1000

    faster faster

--- plugins/cleaner/cleaner.php
+++ plugins/cleaner/cleaner.php
@@ -144,3 +144,15 @@ if ( ! function_exists( 'redux_disable_dev_mode_plugin' ) ) {
 // REMOVE WP EMOJI
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
+
+// REMOVE WP jQuery 
+add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+function remove_jquery_migrate( &$scripts)
+{
+    if(!is_admin())
+    {
+        $scripts->remove( 'jquery');
+        $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.10.2' );
+    }
+}
