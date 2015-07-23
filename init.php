9cc72eb129047de3c202dfbbd818b26d1d1551f1
Mitchell Bray <mitchellbray@Mitchells-MacBook-Pro.local>
2015-07-23 16:13:58 +1000

    re enable posts and media

--- init.php
+++ init.php
@@ -4,7 +4,7 @@
  *	Plugin Name: Pressed Core
  *	Plugin URI: http://pressedsites.com/
  *	Description: helper functions and classes for building great wordpress themes.
- *	Version: 1.0.2
+ *	Version: 1.0.3
  *	Author: Mitchell Bray
  *	Author URI: http://webcreationcentre.com.au/
  *	GitHub Plugin URI:	unetics/ps_
